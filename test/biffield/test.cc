5d91bbaccd3606073fb2975611e323d5a73e3d8a
Cliff L. Biffle <code@cliffle.com>
2014-03-03 04:39:31 -0800

    biffields static test

--- test/biffield/test.cc
+++ test/biffield/test.cc
@@ -4,6 +4,24 @@
 
 Example ex;
 
+/*
+ * Compile-time test cases.
+ */
+
+// Value types must be literal types.
+static constexpr auto foo_v = Example::foo_value_t()
+                              .with_top(0xDEAD)
+                              .with_bottom(0xBEEF);
+
+static_assert(foo_v.get_all() == 0xDEADBEEF,
+              "Fields must assemble as specified.");
+static_assert(foo_v.get_all() == static_cast<uint32_t>(foo_v),
+              "Cast operator must exist and return all bits.");
+
+/*
+ * Dynamic test cases.
+ */
+
 TEST(Biffield, Basic) {
   ex.write_foo(Example::foo_value_t()
                .with_top(0xDEAD)
