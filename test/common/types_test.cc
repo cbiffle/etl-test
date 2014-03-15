9274e6e5c3253c71e25769e869bec9288cec0a91
Cliff L. Biffle <code@cliffle.com>
2014-03-15 10:06:31 -0700

    test/common/types_test

---
+++ test/common/types_test.cc
@@ -0,0 +1,25 @@
+#include "etl/common/types.h"
+#include "etl/common/type_traits.h"
+
+#include <cstddef>
+
+using etl::common::IsSame;
+using etl::common::IsUnqualifiedIntegral;
+using etl::common::IsUnsigned;
+using etl::common::Size;
+
+/*
+ * Since Size is intended to parallel size_t, we test the following properties
+ * of size_t:
+ *
+ *  "std::size_t is the unsigned integer type of the result of the sizeof
+ *   operator and the alignof operator."
+ */
+static_assert(IsUnqualifiedIntegral<Size>::value, "Size must be integral.");
+static_assert(IsUnsigned<Size>::value, "Size must be unsigned.");
+static_assert(IsSame<Size, decltype(sizeof(char(0)))>::value,
+              "Size must be the type of sizeof.");
+static_assert(IsSame<Size, decltype(alignof(char(0)))>::value,
+              "Size must be the type of alignof.");
+
+int main() { return 0; }
