075c4e0f08827920fb9e35bd1b26d7991df092a3
Cliff L. Biffle <code@cliffle.com>
2014-11-19 09:04:56 -0800

    type_traits_test: test size-based type selection.

--- test/type_traits_test.cc
+++ test/type_traits_test.cc
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <cstdint>
 #include <type_traits>
 
 #include "etl/type_traits.h"
@@ -40,3 +42,35 @@ CHECK_TYPE_TRANSFER(int const volatile, bool, etl::MatchQualifiers,
                     bool const volatile);
 
 #undef CHECK_TYPE_TRANSFER
+
+
+/*
+ * Size-based type selection.  All these tests make some assumptions about the
+ * platform.  First, we assume that 8 and 16 bit types are available at all,
+ * which isn't true for old RISCs and modern DSPs.  Second, we assume that these
+ * types can be distinguished using sizeof, which tends to be false for the same
+ * systems.
+ */
+
+static_assert(CHAR_BIT == 8, "These tests assume an eight-bit char.");
+static_assert(sizeof(uint8_t) == 1, "uint8_t should occupy one char.");
+static_assert(sizeof(uint16_t) == 2, "uint16_t should occupy two chars.");
+static_assert(sizeof(uint32_t) == 4, "uint32_t should occupy four chars.");
+
+template <std::size_t N>
+using Ints = etl::SelectBySize<N, uint8_t, uint16_t, uint32_t>;
+
+// Make sure the types come back out.
+static_assert(std::is_same<Ints<1>, uint8_t>::value, "");
+static_assert(std::is_same<Ints<2>, uint16_t>::value, "");
+static_assert(std::is_same<Ints<4>, uint32_t>::value, "");
+
+static_assert(std::is_same<int8_t, etl::SignedIntOfSize<1>>::value, "");
+static_assert(std::is_same<int16_t, etl::SignedIntOfSize<2>>::value, "");
+static_assert(std::is_same<int32_t, etl::SignedIntOfSize<4>>::value, "");
+static_assert(std::is_same<int64_t, etl::SignedIntOfSize<8>>::value, "");
+
+static_assert(std::is_same<uint8_t, etl::UnsignedIntOfSize<1>>::value, "");
+static_assert(std::is_same<uint16_t, etl::UnsignedIntOfSize<2>>::value, "");
+static_assert(std::is_same<uint32_t, etl::UnsignedIntOfSize<4>>::value, "");
+static_assert(std::is_same<uint64_t, etl::UnsignedIntOfSize<8>>::value, "");
