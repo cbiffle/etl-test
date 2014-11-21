8eeee8e4ddda6b144d43acbd72b7d2b98e8b0e58
Cliff L. Biffle <code@cliffle.com>
2014-11-21 10:03:35 -0800

    data/crc32_test: test incremental computation.

--- test/data/crc32_test.cc
+++ test/data/crc32_test.cc
@@ -16,3 +16,19 @@ TEST(Crc32, CatalogueCheck) {
 
   ASSERT_EQ(0xcbf43926, result);
 }
+
+TEST(Crc32, CatalogueCheckTwoParts) {
+  /*
+   * This checks the implementation against the value noted in the Catalogue of
+   * Parametrised CRC Algorithms:
+   *  http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat.crc-32
+   *
+   * We do it in two chunks to verify chaining behavior.
+   */
+  std::uint8_t const input1[] { '1', '2', '3', '4' };
+  std::uint8_t const input2[] { '5', '6', '7', '8', '9' };
+
+  auto result = crc32(input2, crc32(input1));
+
+  ASSERT_EQ(0xcbf43926, result);
+}
