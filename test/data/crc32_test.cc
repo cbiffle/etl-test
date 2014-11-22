eb3cb792e831c47a8b198598472199baddca0621
Cliff L. Biffle <code@cliffle.com>
2014-11-22 11:15:09 -0800

    data/crc32_test: test all table variations.

--- test/data/crc32_test.cc
+++ test/data/crc32_test.cc
@@ -1,34 +1,64 @@
 #include "etl/data/crc32.h"
+#include "etl/data/crc32_impl.h"
 
 #include <gtest/gtest.h>
 
-using etl::data::crc32;
+namespace etl {
+namespace data {
 
-TEST(Crc32, CatalogueCheck) {
-  /*
-   * This checks the implementation against the value noted in the Catalogue of
-   * Parametrised CRC Algorithms:
-   *  http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat.crc-32
-   */
-  std::uint8_t const input[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+/*
+ * We want to evaluate the Crc32 implementation at all supported table sizes.
+ * GTest does not support parameterizing a test case by a *compile-time* value,
+ * only a runtime value or a compile-time type.
+ *
+ * Fortunately, we can lift values to types.  std::integral_constant to the
+ * rescue!
+ */
 
-  auto result = crc32(input);
+template <typename N>
+class Crc32Test : public ::testing::Test,
+                  public Crc32Table<N::value> {};
 
-  ASSERT_EQ(0xcbf43926, result);
+using TestTypes = ::testing::Types<
+  std::integral_constant<std::size_t, 1>,
+  std::integral_constant<std::size_t, 2>,
+  std::integral_constant<std::size_t, 4>,
+  std::integral_constant<std::size_t, 8>
+>;
+TYPED_TEST_CASE(Crc32Test, TestTypes);
+
+using Data = RangePtr<std::uint8_t const>;
+
+/*
+ * The CatalogueChecks below test the implementation against the value noted in
+ * The Catalogue of Parametrised CRC Algorithms:
+ *  http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat.crc-32
+ */
+
+static constexpr std::uint8_t catalogue_vector[] {
+  '1', '2', '3', '4', '5', '6', '7', '8', '9'
+};
+static constexpr std::uint32_t catalogue_result = 0xcbf43926;
+
+TYPED_TEST(Crc32Test, CatalogueCheckAllAtOnce) {
+  ASSERT_EQ(catalogue_result, this->process(catalogue_vector));
 }
 
-TEST(Crc32, CatalogueCheckTwoParts) {
-  /*
-   * This checks the implementation against the value noted in the Catalogue of
-   * Parametrised CRC Algorithms:
-   *  http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat.crc-32
-   *
-   * We do it in two chunks to verify chaining behavior.
-   */
-  std::uint8_t const input1[] { '1', '2', '3', '4' };
-  std::uint8_t const input2[] { '5', '6', '7', '8', '9' };
+TYPED_TEST(Crc32Test, CatalogueCheckCharAtATime) {
+  Data v = catalogue_vector;
+  std::uint32_t result = 0;
+  for (unsigned i = 0; i < v.count(); ++i) {
+    result = this->process(v.slice(i, i + 1), result);
+  }
 
-  auto result = crc32(input2, crc32(input1));
-
-  ASSERT_EQ(0xcbf43926, result);
+  ASSERT_EQ(catalogue_result, result);
 }
+
+TYPED_TEST(Crc32Test, CatalogueCheckTwoChunks) {
+  Data v = catalogue_vector;
+  ASSERT_EQ(catalogue_result,
+      this->process(v.tail_from(4), this->process(v.slice(0, 4))));
+}
+
+}  // namespace data
+}  // namespace etl
