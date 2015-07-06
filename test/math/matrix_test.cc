cc031f7dad31db89489cfd65cb0400954915a0d3
Cliff L. Biffle <code@cliffle.com>
2015-07-06 13:29:59 -0700

    test/math/matrix_test: update for new literal syntax.

--- test/math/matrix_test.cc
+++ test/math/matrix_test.cc
@@ -13,54 +13,54 @@ namespace math {
  */
 
 static constexpr auto id2 = Matrix<2, 2, int>::identity();
-static_assert(id2.rows[0] == transpose(Vec2i{1, 0}), "");
-static_assert(id2.rows[1] == transpose(Vec2i{0, 1}), "");
+static_assert(id2.row[0] == transpose(Vec2i{1, 0}), "");
+static_assert(id2.row[1] == transpose(Vec2i{0, 1}), "");
 
 static_assert(id2 * Vec2i{3, 4} == Vec2i{3, 4}, "");
 
-static constexpr auto a = Matrix<3, 2, int> {{
+static constexpr auto a = Matrix<3, 2, int> {
   {4, 8},
   {0, 2},
   {1, 6},
-}};
+};
 
-static constexpr auto b = Matrix<2, 2, int> {{
+static constexpr auto b = Matrix<2, 2, int> {
   {5, 2},
   {9, 4},
-}};
+};
 
-static constexpr auto expected_ab = Matrix<3, 2, int> {{
+static constexpr auto expected_ab = Matrix<3, 2, int> {
   {92, 40},
   {18, 8},
   {59, 26},
-}};
+};
 
 static_assert(a * b == expected_ab, "");
 
-static constexpr auto expected_trans_a = Matrix<2, 3, int> {{
+static constexpr auto expected_trans_a = Matrix<2, 3, int> {
   {4, 0, 1},
   {8, 2, 6},
-}};
+};
 
 static_assert(transpose(a) == expected_trans_a, "");
 
 
 
-static constexpr auto expected_translate = Matrix<4, 4, int> {{
+static constexpr auto expected_translate = Matrix<4, 4, int> {
   {1, 0, 0, 3},
   {0, 1, 0, 4},
   {0, 0, 1, 5},
   {0, 0, 0, 1},
-}};
+};
 
 static_assert(translate(Vec3f{3, 4, 5}) == expected_translate, "");
 
-static constexpr auto expected_scale = Matrix<4, 4, int> {{
+static constexpr auto expected_scale = Matrix<4, 4, int> {
   {10, 0, 0, 0},
   {0, 20, 0, 0},
   {0, 0, 30, 0},
   {0, 0, 0, 1},
-}};
+};
 static_assert(scale(Vec3f{10,20,30}) == expected_scale, "");
 
 }  // namespace math
